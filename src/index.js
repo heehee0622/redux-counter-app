c4c42ef59cc4a4cfd00baa50af055de3b040c950
heehee0622 <omen444@gmail>
2021-01-28 22:24:24 +0900

    컴포넌트

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
+//리덕스 관련 불러오기
+import {createStore} from 'redux';
+import reducers from './reducers';
+import {Provider} from 'react-redux';
+//store  객체 생성하면서 Reducers를 전달해야 함.
+const store = createStore(reducers,  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
+//React App 컴포넌트에서  store 객체를 사용할 수 있도록 주입 시켜주어야함
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
